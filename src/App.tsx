219d7718c6ec7852f25fcd5bc3980874a6332d13
simonas <simonas@teachersleadtech.com>
2022-11-21 22:41:59 +0200

    Add the timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import differenceInMinutes from 'date-fns/differenceInMinutes';
+
 import './App.css';
+import useInterval from './useInterval';
 
 function App() {
+  const { t } = useTranslation();
+  const startDate = new Date("Nov 20 2022 01:00:00 GMT+0200");
+
+  const [minutesSince, setMinutesSince] = useState<number>(differenceInMinutes(Date.now(), startDate));
+  const hoursSince = Math.floor(minutesSince / 60);
+  const daysSince = Math.floor(hoursSince / 24);
+
+  useInterval(() => {
+    setMinutesSince(differenceInMinutes(Date.now(), startDate));
+  }, 1000 * 10);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {t('days', { count: daysSince })}, {t('hours', { count: hoursSince - daysSince * 24 })} and {t('minutes', { count: minutesSince - hoursSince * 60 })} have passed.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+import './i18n';
+
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
